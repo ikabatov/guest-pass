51519162f78b25e83d71e2a38eb97bccf028ed5e
Ilya Kabatov <zee.teefa@yandex.ru>
2026-09-23 17:33:30 +0300

    Add initial commit

---
+++ src/main/java/ru/ik/guestpass/GuestPassApplication.java
@@ -0,0 +1,13 @@
+package ru.ik.guestpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ik/guestpass/GuestPassApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ik.guestpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
